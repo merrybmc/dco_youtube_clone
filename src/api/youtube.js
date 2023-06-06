6219ce3e9a21c8120065593de554229a82505d55
Byeong Min Cho <merrybmc@gmail.com>
2023-06-06 22:18:52 +0900

    비디오 상세 페이지

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -9,6 +9,10 @@ export default class Youtube {
     return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
   }
 
+  async channelImageURL(id) {
+    return this.apiClient.channels({ params: { part: 'snippet', id } }).then((response) => response.data.items[0].snippet.thumbnails.default.url);
+  }
+
   async #searchByKeyword(keyword) {
     return this.apiClient
       .search({ params: { part: 'snippet', maxResults: 25, type: 'video', q: keyword } })
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { formatAgo } from '../util/date';
+import { useNavigate } from 'react-router-dom';
 
 export default function VideoCard({ video }) {
+  const navigate = useNavigate();
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
 
   return (
-    <li>
+    <li className='cursor-pointer' onClick={() => navigate(`videos/watch/${video.id}`, { state: { video } })}>
       <img className='w-full' src={thumbnails.medium.url} alt={title} />
       <div>
         <p className='font-semibold my-2 line-clamp-2'>{title}</p>
